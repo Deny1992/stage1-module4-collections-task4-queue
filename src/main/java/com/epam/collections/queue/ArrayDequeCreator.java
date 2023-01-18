8ac2ad22e87f50464492da33148104f2707c70b4
Denyonepeny <denisshulga1992@gmail.com>
2023-01-18 15:55:08 +0300

    done

--- src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
+++ src/main/java/com/epam/collections/queue/ArrayDequeCreator.java
@@ -1,10 +1,26 @@
 package com.epam.collections.queue;
 
-import java.util.ArrayDeque;
-import java.util.PriorityQueue;
-import java.util.Queue;
+import java.util.*;
 
 public class ArrayDequeCreator extends PriorityQueue<String> {
     public ArrayDeque<Integer> createArrayDeque(Queue<Integer> firstQueue, Queue<Integer> secondQueue) {
+        ArrayDeque<Integer> deque = new ArrayDeque<>();
+        LinkedList<Integer> first = (LinkedList<Integer>) firstQueue;
+        LinkedList<Integer> second = (LinkedList<Integer>) secondQueue;
+        deque.add(Objects.requireNonNull(first.poll()));
+        deque.add(Objects.requireNonNull(first.poll()));
+        deque.add(Objects.requireNonNull(second.poll()));
+        deque.add(Objects.requireNonNull(second.poll()));
+
+        while (first.peek() != null){
+            first.addLast(deque.pollLast());
+            deque.add(Objects.requireNonNull(first.poll()));
+            deque.add(Objects.requireNonNull(first.poll()));
+            second.addLast(deque.pollLast());
+            deque.add(Objects.requireNonNull(second.poll()));
+            deque.add(Objects.requireNonNull(second.poll()));
+        }
+
+        return deque;
     }
 }
--- src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
+++ src/main/java/com/epam/collections/queue/DishOrderDeterminer.java
@@ -1,8 +1,28 @@
 package com.epam.collections.queue;
 
-import java.util.List;
+
+
+import java.util.*;
 
 public class DishOrderDeterminer {
     public List<Integer> determineDishOrder(int numberOfDishes, int everyDishNumberToEat) {
+        ArrayList<Integer> result = new ArrayList<>();
+        LinkedList<Integer> list = new LinkedList<>();
+
+        for(int i = 1; i <= numberOfDishes; i++){
+            list.add(i);
+        }
+        int counter = 1;
+        while (list.size() > 0){
+            if(counter % everyDishNumberToEat == 0){
+                counter = 1;
+                result.add(list.poll());
+            }else {
+                list.add(list.poll());
+                counter++;
+            }
+        }
+
+        return result;
     }
 }
